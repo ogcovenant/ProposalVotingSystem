843813d4030ab5a7c748e4bed46abe9287fc6537
ogcovenant <justcovenant@outlook.com>
2024-07-22 11:39:59 +0100

    feat: finished the basic contract

--- contracts/ProposalContract.sol
+++ contracts/ProposalContract.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
-pragma solidity ^0.8.26;
 
 contract ProposalContract {
-    uint256 private counter;
+
     address owner;
+    uint256 private counter;
+
 
     struct Proposal {
-        string title;
         string description;
         uint256 approve;
         uint256 reject;
@@ -17,36 +18,130 @@ contract ProposalContract {
         bool is_active;
     }
 
+
     mapping(uint256 => Proposal) proposal_history;
+
+
+    address[] private voted_addresses; 
+
 
     constructor() {
         owner = msg.sender;
+        voted_addresses.push(msg.sender);
     }
 
+
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner of this contract can create a proposal");
+        require(msg.sender == owner);
         _;
     }
 
-    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end)
-        external onlyOwner
-    {
-        counter += 1;
-        proposal_history[counter] = Proposal(
-            _title,
-            _description,
-            0,
-            0,
-            0,
-            _total_vote_to_end,
-            false,
-            true
-        );
+
+    modifier active() {
+        require(proposal_history[counter].is_active == true);
+        _;
     }
 
-    function changeOwner( address new_owner ) external onlyOwner {
+
+    modifier newVoter(address _address) {
+        require(!isVoted(_address), "Address has not voted yet");
+        _;
+    }
+
+
+    function setOwner(address new_owner) external onlyOwner {
         owner = new_owner;
     }
 
+
+    function create(string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
+        counter += 1;
+        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
+    }
     
+
+
+    function vote(uint8 choice) external active newVoter(msg.sender){
+        Proposal storage proposal = proposal_history[counter];
+        uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
+
+
+        voted_addresses.push(msg.sender);
+
+
+        if (choice == 1) {
+            proposal.approve += 1;
+            proposal.current_state = calculateCurrentState();
+        } else if (choice == 2) {
+            proposal.reject += 1;
+            proposal.current_state = calculateCurrentState();
+        } else if (choice == 0) {
+            proposal.pass += 1;
+            proposal.current_state = calculateCurrentState();
+        }
+
+
+        if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
+            proposal.is_active = false;
+            voted_addresses = [owner];
+        }
+    }
+
+
+    function terminateProposal() external onlyOwner active {
+        proposal_history[counter].is_active = false;
+    }
+
+
+
+
+    function calculateCurrentState() private view returns(bool) {
+        Proposal storage proposal = proposal_history[counter];
+
+
+        uint256 approve = proposal.approve;
+        uint256 reject = proposal.reject;
+        uint256 pass = proposal.pass;
+        
+        if (proposal.pass %2 == 1) {
+            pass += 1;
+        }
+
+
+        pass = pass / 2;
+
+
+        if (approve > reject + pass) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+    // ****************** Query Functions ***********************
+
+
+    function isVoted(address _address) public view returns (bool) {
+        for (uint i = 0; i < voted_addresses.length; i++) {
+            if (voted_addresses[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    function getCurrentProposal() external view returns(Proposal memory) {
+        return proposal_history[counter];
+    }
+
+
+    function getProposal(uint256 number) external view returns(Proposal memory) {
+        return proposal_history[number];
+    }
 }
