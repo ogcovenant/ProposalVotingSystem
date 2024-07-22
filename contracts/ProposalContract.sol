8b7dd63ec9724701f69f6aeba3311c91c5cedf19
ogcovenant <justcovenant@outlook.com>
2024-07-22 10:39:26 +0100

    added the create proposal function

--- contracts/ProposalContract.sol
+++ contracts/ProposalContract.sol
@@ -3,17 +3,34 @@
 pragma solidity ^0.8.26;
 
 contract ProposalContract {
+    uint256 private counter;
 
     struct Proposal {
         string title;
         string description;
         uint256 approve;
         uint256 reject;
-        uint256 pass; 
+        uint256 pass;
         uint256 total_vote_to_end;
         bool current_state;
         bool is_active;
     }
 
     mapping(uint256 => Proposal) proposal_history;
+
+    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end)
+        external
+    {
+        counter += 1;
+        proposal_history[counter] = Proposal(
+            _title,
+            _description,
+            0,
+            0,
+            0,
+            _total_vote_to_end,
+            false,
+            true
+        );
+    }
 }
